9b8175ca607fffba9e3258ad8918b913b15dc075
Joel Ward <jmward@gmail.com>
2016-02-13 10:05:15 -0800

    fill out writes

--- cwrops.py
+++ cwrops.py
@@ -9,23 +9,22 @@ class WriteOps(Operations):
         self.files[path]['st_mode'] &= 0770000
         self.files[path]['st_mode'] |= mode
         Exec("UPDATE inodes SET meta['st_mode']=%s WHERE pathname=%s", (mode, self.pfx+'/'+path))
-        #os.chmod(self.pfx+path, mode)
     def chown(self, path, uid, gid):
         print "CHOWN", path, uid, gid
         self.files[path]['st_uid'] = uid
         self.files[path]['st_gid'] = gid
         Exec("UPDATE inodes SET meta['st_uid']=%s WHERE pathname=%s", (uid, self.pfx+'/'+path))
         Exec("UPDATE inodes SET meta['st_gid']=%s WHERE pathname=%s", (gid, self.pfx+'/'+path))
-        #os.chown(self.pfx+path, uid, gid)
     def utimens(self, path, times=None):
         print "UTIMENS", path, times
         now = time()
         atime, mtime = times if times else (now, now)
         self.files[path]['st_atime'] = atime
         self.files[path]['st_mtime'] = mtime
+        atime = int(atime)
+        mtime = int(mtime)
         Exec("UPDATE inodes SET meta['st_atime']=%s WHERE pathname=%s", (atime, self.pfx+'/'+path))
         Exec("UPDATE inodes SET meta['st_mtime']=%s WHERE pathname=%s", (mtime, self.pfx+'/'+path))
-        #os.utime(self.pfx+path, times)
     def rename(self, path, new):
         print "RENAME", path, new
         self.files[new] = self.files.pop(path)
@@ -35,28 +34,30 @@ class WriteOps(Operations):
         self.files.pop(path)
         self.files['/']['st_nlink'] -= 1
         Exec("DELETE FROM inodes WHERE pathname=%s", (self.pfx+'/'+path,))
-        #os.rmdir(self.pfx+path)
     def truncate(self, path, length, fh=None):
         print "TRUNC", path, length, fh
         self.data[path] = self.data[path][:length]
         self.files[path]['st_size'] = length
+        rs = Exec("SELECT data FROM filedata WHERE pathname=%s", (self.pfx+'/'+path,))
+        print "RST", rs
+        data = self.data[path]
+        Exec("UPDATE filedata SET data=%s WHERE pathname=%s", (data, self.pfx+'/'+path,))
         #with open(self.pfx+path,'w+') as f:
         #    f.truncate(length)
     def unlink(self, path):
         print "UNLINK", path
         self.files.pop(path)
-        Exec("DELETE FROM inodes WHERE pathname=%s", (self.pfx+'/'+path,))
-        #os.unlink(self.pfx+path)
+        Exec("DELETE FROM  inodes  WHERE pathname=%s", (self.pfx+'/'+path,))
+        Exec("DELETE FROM filedata WHERE pathname=%s", (self.pfx+'/'+path,))
     def symlink(self, path, source):
         print "SYMLINK", path, source
         self.mk_node(path,'L', sz=len(source))
         self.data[path] = source
-        #os.symlink(self.pfx+source, self.pfx+path)
+        Exec("INSERT INTO filedata data=%s WHERE pathname=%s", (source, self.pfx+'/'+path,))
     def mkdir(self, path, mode):
         print "MKDIR", path, mode
         self.mk_node(path,'D',mode)
         self.files['/']['st_nlink'] += 1
-        #os.mkdir(self.pfx+path, mode)
     def create(self, path, mode):
         print "CREATE", path, mode
         self.mk_node(path,'F',mode)
@@ -75,4 +76,15 @@ class WriteOps(Operations):
         #with open(self.pfx+path,'w+') as f:
         #    f.seek(offset, 0)
         #    ret = f.write(fh, data)
+        rs = Exec("SELECT data FROM filedata WHERE pathname=%s",
+                  (self.pfx+'/'+path,))
+        print "RSW", rs
+        for x in rs:
+            print "XXX", x
+            pass
+        data = self.data[path]
+        Exec("UPDATE  inodes  SET meta['st_size']=%s WHERE pathname=%s",
+             (len(data), self.pfx+'/'+path,))
+        Exec("UPDATE filedata SET data=%s            WHERE pathname=%s",
+             (data, self.pfx+'/'+path,))
         return len(data)
