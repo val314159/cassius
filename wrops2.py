5a63d3e965a3091e8880ee40508bb6cbaca82bcd
Joel Ward <jmward@gmail.com>
2016-02-13 16:50:58 -0800

    take out old stuff

--- wrops2.py
+++ wrops2.py
@@ -5,46 +5,34 @@ from cass import Exec
 class WriteOps(Operations):
     def chmod(self, path, mode):
         print "CHMOD", path, mode
-        self.files[path]['st_mode'] &= 0770000
-        self.files[path]['st_mode'] |= mode
         Exec("UPDATE inodes SET meta['st_mode']=%s WHERE pathname=%s", (mode, self.pfx+'/'+path))
     def chown(self, path, uid, gid):
         print "CHOWN", path, uid, gid
-        self.files[path]['st_uid'] = uid
-        self.files[path]['st_gid'] = gid
         Exec("UPDATE inodes SET meta['st_uid']=%s WHERE pathname=%s", (uid, self.pfx+'/'+path))
         Exec("UPDATE inodes SET meta['st_gid']=%s WHERE pathname=%s", (gid, self.pfx+'/'+path))
     def utimens(self, path, times=None):
         print "UTIMENS", path, times
         now = time()
         atime, mtime = times if times else (now, now)
-        self.files[path]['st_atime'] = atime
-        self.files[path]['st_mtime'] = mtime
         atime = int(atime)
         mtime = int(mtime)
         Exec("UPDATE inodes SET meta['st_atime']=%s WHERE pathname=%s", (atime, self.pfx+'/'+path))
         Exec("UPDATE inodes SET meta['st_mtime']=%s WHERE pathname=%s", (mtime, self.pfx+'/'+path))
     def rename(self, path, new):
         print "RENAME", path, new
-        self.files[new] = self.files.pop(path)
     def rmdir(self, path):
         print "RMDIR", path
-        self.files.pop(path)
-        self.files['/']['st_nlink'] -= 1
         Exec("DELETE FROM inodes WHERE pathname=%s", (self.pfx+'/'+path,))
     def unlink(self, path):
         print "UNLINK", path
-        self.files.pop(path)
         Exec("DELETE FROM  inodes  WHERE pathname=%s", (self.pfx+'/'+path,))
         Exec("DELETE FROM filedata WHERE pathname=%s", (self.pfx+'/'+path,))
     def symlink(self, path, source):
         print "SYMLINK", path, source
         self.mk_node(path,'L', sz=len(source))
-        self.data[path] = source
     def mkdir(self, path, mode):
         print "MKDIR", path, mode
         self.mk_node(path,'D',mode)
-        self.files['/']['st_nlink'] += 1
     def create(self, path, mode):
         print "CREATE", path, mode
         self.mk_node(path,'F',mode)
@@ -56,26 +44,15 @@ class WriteOps(Operations):
         return self.fd
     def truncate(self, path, length, fh=None):
         print "TRUNC", path, length, fh
-        self.data[path] = self.data[path][:length]
-        self.files[path]['st_size'] = length
-
         old_data = self._read_all(path, fh)
         old_data = old_data[:length]
         self._write_all(path, old_data)
-
     def write(self, path, data, offset, fh):
         print "WRITE", path, data, offset, fh
-        sdata = self.data[path]
         endp = offset+len(data)
-        sdata = sdata[:offset] + data + sdata[endp:]
-        self.data[path] = sdata
-        
-        self.files[path]['st_size'] = len(self.data[path])
-
         old_data = self._read_all(path, fh)
         old_data = old_data[:offset] + data + old_data[endp:]
         self._write_all(path, old_data)
-
         return len(data)
     def _read_all(self, path, fh):
         rs = Exec("SELECT data FROM filedata WHERE pathname=%s", (self.pfx+'/'+path,))
